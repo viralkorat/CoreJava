77b2bff0a10ec0278d9d712c55367be5121f3984
vkorat <vkorat@CATOR-VKORAT1.armstrong.local>
2022-08-31 14:30:46 -0400

    My programm

---
+++ src/OddEven.java
@@ -0,0 +1,15 @@
+
+public class OddEven {
+
+	public static void main(String[] args) {
+	
+	int num=100;
+	
+	if (num % 2 == 0)
+		System.out.println("even number selected");
+	else
+		System.out.println("odd number selected");
+
+	}
+
+}
