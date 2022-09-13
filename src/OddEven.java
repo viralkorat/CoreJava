0beae913145524fb1bcfe0774dd25df35cefd384
viralkorat <koratviral8896@gmail.com>
2022-09-13 18:23:04 -0400

    add

--- src/OddEven.java
+++ src/OddEven.java
@@ -1,15 +1,20 @@
+import java.util.Scanner;
 
 public class OddEven {
 
 	public static void main(String[] args) {
 	
-	int num=100;
-	
-	if (num % 2 == 0)
-		System.out.println("even number selected");
-	else
-		System.out.println("odd number selected");
-
+		Scanner s = new Scanner (System.in);
+		System.out.println("Please enter your number");
+		
+		int num = s.nextInt();
+		if (num%2 == 0) {
+		
+		System.out.println("Your number is even");
+		}
+		else {
+			System.out.println("Your number is odd");
+		}
+		}
 	}
 
-}
